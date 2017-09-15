a8df5d12778c3cbf453189b6742376edda4a0bcb
Nicolas F <nicolas.funke@amplifierstrategies.com>
2017-09-15 14:30:39 -0700

    test build fix

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-echo '### Interpolating Variables ###'
-sed -i 's/TAC_PREVIEW_TOKEN/'"$TAC_PREVIEW_TOKEN"'/g' _config.yml 
-sed -i 's/TAC_CONTENTFUL_ACCESS_TOKEN/'"$TAC_CONTENTFUL_ACCESS_TOKEN"'/g' _config.yml 
-sed -i 's/TAC_CONTENTFUL_SPACE_ID/'"$TAC_CONTENTFUL_SPACE_ID"'/g' _config.yml 
-echo '### Fetching content & Building site ###'
-jekyll contentful --rebuild
-echo '### DONE ###'
-echo '### Removing Variables from config file ###'
+echo '### Fetching content ###'
+jekyll contentful
+echo '### Building site ###'
+jekyll build
 echo '### DONE ###'
